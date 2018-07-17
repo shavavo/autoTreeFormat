08e7b3b204bcf88fdb0e0a0c8076dcd97fd709b5
David Cheng <dc273@duke.edu>
2018-07-17 16:17:40 -0400

    Comments

--- autoTree.py
+++ autoTree.py
@@ -49,9 +49,23 @@ def create_tree(current_dict, top_dir, prefix="", sort_alpha=True):
         connector = '' if item == top_dir else '└── ' if last else '├── '
         nextPrefix = prefix + ('' if item == top_dir else '│   ' if not last else '    ')
         new_row = create_tree(current_dict[item], top_dir, prefix=nextPrefix, sort_alpha=sort_alpha)
+
         output += prefix + connector + item + '\n' + new_row
 
     return output
+
+
+def add_comment(output):
+    new_output = ""
+
+    output_split = output.split('\n')[:-1]
+    padding = max([len(x) for x in output_split]) + 10
+
+    for x in output_split:
+        new_output += x + (padding - len(x)) * " " + "#\n"
+
+    return new_output
+
 
 
 def main():
@@ -70,10 +84,16 @@ def main():
     parser.add_argument('-a', dest="sort_alpha", type=str2bool, default=[True], nargs=1,
                         help="Sort files alphabetically. If set to False, will order in natural progression.")
 
+    parser.add_argument('-c', dest="comment_fields", type=str2bool, default=[True], nargs=1,
+                        help="Add comment fields for each file")
+
     args = parser.parse_args()
 
     file_dict = get_directory_structure(args.file[0], show_sys_files=args.include_system_files[0], blacklist=args.blacklist)
     output = create_tree(file_dict, list(file_dict.keys())[0], sort_alpha=args.sort_alpha[0])
+
+    if args.comment_fields[0]:
+        output = add_comment(output)
 
     path = args.file[0] + '/' + args.text_file_name[0]
     with open(path, "w") as text_file:
